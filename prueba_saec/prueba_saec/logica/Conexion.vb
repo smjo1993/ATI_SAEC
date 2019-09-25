da749b490f27b95a77a461bc010ff3975b986f92
sebastian <sebastian@DESKTOP-I0LML3S>
2019-09-25 15:52:38 -0400

    added: se agrego conexion a la base de datos

---
+++ prueba_saec/prueba_saec/logica/Conexion.vb
@@ -0,0 +1,5 @@
+﻿Public Class Conexion
+    Public Shared Function strSQLSERVER() As String
+        Return "Server=192.168.124.9;DataBase=BD_SAEC_TEST;Password=user_saec_test;User ID=user_saec_test;"
+    End Function
+End Class
