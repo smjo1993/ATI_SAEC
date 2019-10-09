a8df40a980839ed2228cac3c53d9508facb056bc
fcaimanque <fernando.caimanque@alumnos.ucn.cl>
2019-10-09 17:51:19 -0300

    Added: -Cards para vista agregarDcto
    Deleted:
    -agregarDocumento

--- prueba_saec/prueba_saec/logica/clsDocumento.vb
+++ prueba_saec/prueba_saec/logica/clsDocumento.vb
@@ -26,5 +26,20 @@ Public Class clsDocumento
     '        con.Dispose()
     '    End Try
     'End Function
+    'obtenerTipo
+    Public Function obtenerDocumento()
 
+        Dim con As New SqlConnection(Conexion.strSQLSERVER)
+        Dim ds As New DataSet()
+
+        Dim sql As String = "SP_SAEC_ListarDocumentos"
+
+        con.Open()
+
+        Dim dbDataAdapter = New Data.SqlClient.SqlDataAdapter(sql, con)
+        dbDataAdapter.Fill(ds)
+
+        Return ds.Tables(0)
+
+    End Function
 End Class
