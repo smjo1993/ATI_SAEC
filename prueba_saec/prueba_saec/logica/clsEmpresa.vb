4c192d61e57b690abf8dfcda873cd02aa903c41d
JorqueraFrancisco <33900932+JorqueraFrancisco@users.noreply.github.com>
2019-10-09 06:45:06 -0700

    se agrega listar empresas  en la vista y metodos en clsEMpresa

--- prueba_saec/prueba_saec/logica/clsEmpresa.vb
+++ prueba_saec/prueba_saec/logica/clsEmpresa.vb
@@ -19,4 +19,35 @@ Public Class clsEmpresa
         Me.RutContratista = rutContratista
 
     End Sub
+    Public Sub New()
+    End Sub
+
+    Public Function obtenerEmpresas() As DataTable
+
+        Dim con As New SqlConnection(Conexion.strSQLSERVER)
+        Dim ds As New DataSet()
+        Dim sql As String = "SP_SAEC_ListarEmpresas"
+        con.Open()
+        Dim dbDataAdapter = New Data.SqlClient.SqlDataAdapter(sql, con)
+        dbDataAdapter.Fill(ds)
+
+        Return ds.Tables(0)
+
+    End Function
+    Public Function calcularPorcentaje(rutEmpersa As String) As String
+
+        Dim con As New SqlConnection(Conexion.strSQLSERVER)
+        Dim ds As New DataSet()
+        Dim sql As String = "SP_SAEC_CalcularProgresoCarpeta '" & rutEmpersa & "'"
+        con.Open()
+        Dim dbDataAdapter = New Data.SqlClient.SqlDataAdapter(sql, con)
+        dbDataAdapter.Fill(ds)
+
+        Dim aprobados As Integer = ds.Tables(0).Rows(0).Item(0)
+        Dim total As Integer = ds.Tables(1).Rows(0).Item(0)
+        Dim resultado As String = Int((aprobados * 100) / total)
+
+        Return resultado
+
+    End Function
 End Class
