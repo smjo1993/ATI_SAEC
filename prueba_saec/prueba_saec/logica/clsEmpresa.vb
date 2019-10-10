b3645c660c7752f78580ab85eb6abed9df278bc5
JorqueraFrancisco <33900932+JorqueraFrancisco@users.noreply.github.com>
2019-10-10 06:06:50 -0700

    Porcentaje y color del estado listo (faltan pruebas)

--- prueba_saec/prueba_saec/logica/clsEmpresa.vb
+++ prueba_saec/prueba_saec/logica/clsEmpresa.vb
@@ -24,30 +24,46 @@ Public Class clsEmpresa
 
     Public Function obtenerEmpresas() As DataTable
 
-        Dim con As New SqlConnection(Conexion.strSQLSERVER)
-        Dim ds As New DataSet()
         Dim sql As String = "SP_SAEC_ListarEmpresas"
-        con.Open()
-        Dim dbDataAdapter = New Data.SqlClient.SqlDataAdapter(sql, con)
-        dbDataAdapter.Fill(ds)
-
+        Dim ds As DataSet = conexionDb(sql)
         Return ds.Tables(0)
 
     End Function
     Public Function calcularPorcentaje(rutEmpersa As String) As String
 
-        Dim con As New SqlConnection(Conexion.strSQLSERVER)
-        Dim ds As New DataSet()
-        Dim sql As String = "SP_SAEC_CalcularProgresoCarpeta '" & rutEmpersa & "'"
-        con.Open()
-        Dim dbDataAdapter = New Data.SqlClient.SqlDataAdapter(sql, con)
-        dbDataAdapter.Fill(ds)
 
+        Dim sql As String = "SP_SAEC_CalcularProgresoCarpeta '" & rutEmpersa & "'"
+        Dim ds As DataSet = conexionDb(sql)
         Dim aprobados As Integer = ds.Tables(0).Rows(0).Item(0)
         Dim total As Integer = ds.Tables(1).Rows(0).Item(0)
         Dim resultado As String = Int((aprobados * 100) / total)
 
         Return resultado
+
+    End Function
+
+    Public Function obtenerEstado(areaRevisor As Integer, rutEmpersa As String) As Boolean
+
+        Dim sql As String = "SP_SAEC_ListarEstadoDocumento '" & areaRevisor & "','" & rutEmpersa & " '"
+        Dim ds As DataSet = conexionDb(sql)
+        Dim estadoRojo As Integer = ds.Tables(0).Rows.Count
+
+        If estadoRojo > 0 Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+
+    Public Function conexionDb(sql As String) As DataSet
+
+        Dim con As New SqlConnection(Conexion.strSQLSERVER)
+        Dim ds As New DataSet()
+        con.Open()
+        Dim dbDataAdapter = New Data.SqlClient.SqlDataAdapter(sql, con)
+        dbDataAdapter.Fill(ds)
+        Return ds
 
     End Function
 End Class
--- prueba_saec/prueba_saec/presentacion/Funcionarios ATI/verEmpresas.aspx.vb
+++ prueba_saec/prueba_saec/presentacion/Funcionarios ATI/verEmpresas.aspx.vb
@@ -4,11 +4,16 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
         Dim tarjeta As String = ""
-        Dim color As String = "success"
+        Dim color As String
         Dim listaEmpresas As DataTable = crearEmpresas().obtenerEmpresas()
-        Dim porcentaje As Object = crearEmpresas()
+        Dim Empresas As Object = crearEmpresas()
 
         For Each fila As DataRow In listaEmpresas.Rows
+
+            Dim porcentaje As String = Empresas.calcularPorcentaje(fila("rut"))
+            Dim estado As Boolean = Empresas.ObtenerEstado(3, fila("rut"))
+            color = obtenerColor(estado, porcentaje)
+
 
             tarjeta = tarjeta & "   <div Class=""col-xl-3 col-md-6 mb-4""> "
             tarjeta = tarjeta & "         <div Class=""card border-left-" + color + " shadow h-100 py-2""> "
@@ -18,11 +23,11 @@
             tarjeta = tarjeta & "                 <div Class=""text-xs font-weight-bold text-" + color + " text-uppercase mb-1"">" + fila("razonSocial") + "</div> "
             tarjeta = tarjeta & "                 <div Class=""row no-gutters align-items-center""> "
             tarjeta = tarjeta & "                   <div Class=""col-auto""> "
-            tarjeta = tarjeta & "                     <div Class=""h5 mb-0 mr-3 font-weight-bold text-gray-800"">" + porcentaje.calcularPorcentaje(fila("rut")) + "%" + "</div> "
+            tarjeta = tarjeta & "                     <div Class=""h5 mb-0 mr-3 font-weight-bold text-gray-800"">" + porcentaje + "%" + "</div> "
             tarjeta = tarjeta & "                   </div> "
             tarjeta = tarjeta & "                  <div Class=""col""> "
             tarjeta = tarjeta & "                     <div Class=""progress progress-sm mr-2""> "
-            tarjeta = tarjeta & "                       <div Class=""progress-bar bg-" + color + """ role=""progressbar"" style=""width: " + porcentaje.calcularPorcentaje(fila("rut")) + "%" + """ aria-valuenow=""50"" aria-valuemin=""0"" aria-valuemax=""100""></div> "
+            tarjeta = tarjeta & "                       <div Class=""progress-bar bg-" + color + """ role=""progressbar"" style=""width: " + porcentaje + "%" + """ aria-valuenow=""50"" aria-valuemin=""0"" aria-valuemax=""100""></div> "
             tarjeta = tarjeta & "                     </div> "
             tarjeta = tarjeta & "                  </div> "
             tarjeta = tarjeta & "                </div> "
@@ -49,7 +54,23 @@
 
     End Function
 
-    Public Function obtenerColor()
+    Public Function obtenerColor(EstadoRojo As Boolean, porcentaje As String) As String
+
+        If EstadoRojo = True Then
+
+            Return "danger"
+
+        ElseIf EstadoRojo = False Then
+
+            If porcentaje = "100" Then
+                Return "success"
+            Else
+                Return "warning"
+            End If
+
+        End If
+
+
 
     End Function
 
