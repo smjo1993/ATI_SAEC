eae7d40af99f9f5e1571b3037b4bf26150264caa
fcaimanque <fernando.caimanque@alumnos.ucn.cl>
2019-11-08 12:24:38 -0300

    Fixed: - Implementación scripts de template
    Added:
    - verListaDctos

--- prueba_saec/prueba_saec/presentacion/Funcionarios ATI/verListaDctos.aspx.designer.vb
+++ prueba_saec/prueba_saec/presentacion/Funcionarios ATI/verListaDctos.aspx.designer.vb
@@ -11,25 +11,7 @@ Option Strict On
 Option Explicit On
 
 
-Partial Public Class modificarDcto
-
-    '''<summary>
-    '''Control dropAreas.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents dropAreas As Global.System.Web.UI.WebControls.DropDownList
-
-    '''<summary>
-    '''Control dropDocumentos.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents dropDocumentos As Global.System.Web.UI.WebControls.DropDownList
+Partial Public Class verListaDctos
 
     '''<summary>
     '''Control CardEdicionDocumento.
