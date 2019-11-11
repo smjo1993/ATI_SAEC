4b7b9156ee7bec09052ad5bce7982b5602970e20
fcaimanque <fernando.caimanque@alumnos.ucn.cl>
2019-11-11 16:14:35 -0300

    FIXED: - modificarDcto: Eliminado checkboxlist de áreas. - clsDocumento: actualizarDocumentos

--- prueba_saec/prueba_saec/presentacion/Funcionarios ATI/verListaDctos.aspx.designer.vb
+++ prueba_saec/prueba_saec/presentacion/Funcionarios ATI/verListaDctos.aspx.designer.vb
@@ -23,60 +23,6 @@ Partial Public Class verListaDctos
     Protected WithEvents gridRequisitos As Global.System.Web.UI.WebControls.GridView
 
     '''<summary>
-    '''Control CardEdicionDocumento.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents CardEdicionDocumento As Global.System.Web.UI.HtmlControls.HtmlGenericControl
-
-    '''<summary>
-    '''Control lblHeadEdicion.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents lblHeadEdicion As Global.System.Web.UI.WebControls.Label
-
-    '''<summary>
-    '''Control TxtNombreDocumentoEdicion.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents TxtNombreDocumentoEdicion As Global.System.Web.UI.WebControls.TextBox
-
-    '''<summary>
-    '''Control dropTipoNuevoDocumento.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents dropTipoNuevoDocumento As Global.System.Web.UI.WebControls.DropDownList
-
-    '''<summary>
-    '''Control chkListaAreasEdicion.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents chkListaAreasEdicion As Global.System.Web.UI.WebControls.CheckBoxList
-
-    '''<summary>
-    '''Control lblAdvertencia.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents lblAdvertencia As Global.System.Web.UI.WebControls.Label
-
-    '''<summary>
     '''Control btnRealizarCambios.
     '''</summary>
     '''<remarks>
