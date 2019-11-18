618340ba56df0b8a6309de591bee6d8e591900fd
sebastian <sebastian@DESKTOP-I0LML3S>
2019-11-18 15:01:57 -0400

    added: clase menu, iconos en la tarjeta

---
+++ prueba_saec/prueba_saec/logica/clsMenu.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Public Class clsMenu
+    Public Function menuUsuarioAti(rut As String, idCarpeta As Integer) As String
+        Dim con As New SqlConnection(Conexion.strSQLSERVER)
+        Console.WriteLine(con.ToString())
+        Try
+
+            Dim ds As New DataSet()
+            Dim sql As String = "SP_SAEC_ObtenerOpcionesMenu '" & rut & "'"
+
+            con.Open()
+            Dim dbDataAdapter = New Data.SqlClient.SqlDataAdapter(sql, con)
+            dbDataAdapter.Fill(ds)
+
+            Dim menu As String = generarMenuATI(ds.Tables(0), idCarpeta)
+            Return menu
+        Catch ex As Exception
+            Dim menu As String = ""
+            Return menu
+        Finally
+            con.Close()
+            con.Dispose()
+        End Try
+    End Function
+    Public Function generarMenuATI(menu As DataTable, idcarpeta As Integer) As String
+        Dim stringMenu As String = ""
+        Dim contadorOpcion As Integer = 0
+        If (menu.Rows.Count > 0) Then
+            For Each opcion As DataRow In menu.Rows
+                If (opcion("opcionPadre") = 0) Then
+                    If (contadorOpcion > 0) Then
+                        stringMenu = stringMenu & "                </div>"
+                        stringMenu = stringMenu & "            </li>"
+                    End If
+                    stringMenu = stringMenu & "            <hr class=""sidebar-divider"">"
+                    stringMenu = stringMenu & "            <li class=""nav-item"">"
+                    stringMenu = stringMenu & "                <a class=""nav-link collapsed"" href=""#"" data-toggle=""collapse"" data-target=""#collapse" + contadorOpcion.ToString + """ aria-expanded=""True"" aria-controls=""collapse" + contadorOpcion.ToString + """>"
+                    stringMenu = stringMenu & "                    <i class=""fas fa-fw fa-cog""></i>"
+                    stringMenu = stringMenu & "                   <span>" + opcion("nombre") + "</span>"
+                    stringMenu = stringMenu & "                </a>"
+                    stringMenu = stringMenu & "                 <div id=""collapse" + contadorOpcion.ToString + " ""class=""collapse"" aria-labelledby=""heading" + contadorOpcion.ToString + """ data-parent=""#accordionSidebar"">"
+                    contadorOpcion += 1
+                Else
+
+                    stringMenu = stringMenu & "                    <div class=""bg-white py-2 collapse-inner rounded"">"
+                    stringMenu = stringMenu & "                        <a class=""collapse-item"" href=" + opcion("link") + ">" + opcion("nombre") + "</a>"
+                    stringMenu = stringMenu & "                    </div>"
+
+                End If
+            Next
+            stringMenu = stringMenu & "            <hr class=""sidebar-divider d-none d-md-block"">"
+            'stringMenu = stringMenu & "            <div class=""text-center d-none d-md-inline"">"
+            'stringMenu = stringMenu & "                <button class=""rounded-circle border-0"" id=""sidebarToggle""></button>"
+            'stringMenu = stringMenu & "            </div>"
+        End If
+        Return stringMenu
+    End Function
+End Class
--- prueba_saec/prueba_saec/presentacion/Funcionarios ATI/verCarpetas.aspx
+++ prueba_saec/prueba_saec/presentacion/Funcionarios ATI/verCarpetas.aspx
@@ -4,34 +4,33 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title></title>
     <link href="../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="../../css/sb-admin-2.min.css" rel="stylesheet">
-
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
-            
-                <!-- Earnings (Monthly) Card Example -->
-           <asp:Label runat="server" ID="lblTarjetaEmpresa" Text=""></asp:Label>
-               <!-- ---------------------------------------- -->
 
-     
+            <!-- Earnings (Monthly) Card Example -->
+            <asp:Label runat="server" ID="lblTarjetaEmpresa" Text=""></asp:Label>
+            <!-- ---------------------------------------- -->
+
+
         </div>
     </form>
 
     <!-- Bootstrap core JavaScript-->
-  <script src="../../vendor/jquery/jquery.min.js"></script>
-  <script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../../vendor/jquery/jquery.min.js"></script>
+    <script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-  <!-- Core plugin JavaScript-->
-  <script src="../../vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="../../vendor/jquery-easing/jquery.easing.min.js"></script>
 
-  <!-- Custom scripts for all pages-->
-  <script src="../../js/sb-admin-2.min.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="../../js/sb-admin-2.min.js"></script>
 
 </body>
 </html>
