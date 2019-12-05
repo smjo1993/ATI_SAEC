229b5bdaf52d4b331f9cba3350d0be880fd15fe1
Thalenus <r.trujillo.vergara@gmail.com>
2019-12-05 17:50:10 -0300

    Fix: Se añade vista crearTrabajador de commit anterior que había quedado fuera

---
+++ prueba_saec/prueba_saec/presentacion/Contratistas/crearTrabajador.aspx.designer.vb
@@ -0,0 +1,87 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class crearTrabajador
+    
+    '''<summary>
+    '''Control lblMenu.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblMenu As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control lblHeadEdicion.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblHeadEdicion As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control TxtRut.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TxtRut As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control TxtNombre.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TxtNombre As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control TxtFono.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TxtFono As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control TxtCorreo.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TxtCorreo As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control lblAdvertencia.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblAdvertencia As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control btnRealizarRegistro.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnRealizarRegistro As Global.System.Web.UI.WebControls.Button
+End Class
