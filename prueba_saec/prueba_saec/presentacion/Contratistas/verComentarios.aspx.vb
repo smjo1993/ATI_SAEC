04b1d8746aa34c4f152cbdaad3990ae88a0fd62a
fcaimanque <fernando.caimanque@alumnos.ucn.cl>
2019-12-13 17:39:43 -0300

    FIXED - ver notificaciones desde vistas con y sin session

--- prueba_saec/prueba_saec/presentacion/Contratistas/verComentarios.aspx.vb
+++ prueba_saec/prueba_saec/presentacion/Contratistas/verComentarios.aspx.vb
@@ -5,13 +5,8 @@ Public Class verComentarios
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Not Page.IsPostBack Then
-            If Session("origen") <> ("confirmarDocumentos.aspx") Then
-                cargarComentariosUrl()
-                cargarMenu()
-            Else
-                cargarComentarios()
-                cargarMenu()
-            End If
+            cargarComentarios()
+            cargarMenu()
         End If
 
     End Sub
@@ -115,115 +110,10 @@ Public Class verComentarios
             Dim rutUsuario As String = usuario.getRut()
             'Dim idCarpeta As Integer = decodificarId()
 
-            Dim stringMenu As String = Menu.menuUsuarioAtiInicio(rutUsuario)
+            Dim stringMenu As String = menu.menuUsuarioAtiInicio(rutUsuario)
             lblMenu.Text = stringMenu
             lblMenu.Visible = True
         End If
-
-    End Sub
-
-    Protected Function decodificarIdDocumento() As Integer
-        Dim idDocCodificado As String = Request.QueryString("i").ToString()
-        Dim data() As Byte = System.Convert.FromBase64String(idDocCodificado)
-        Dim idDocDecodificado As String = System.Text.ASCIIEncoding.ASCII.GetString(data)
-        Dim docuemntoId As Integer = Convert.ToInt32(idDocDecodificado)
-        Return docuemntoId
-    End Function
-
-    Protected Function decodificarIdArea() As Integer
-        Dim idAreaCodificada As String = Request.QueryString("n").ToString()
-        Dim data() As Byte = System.Convert.FromBase64String(idAreaCodificada)
-        Dim idAreaDecodificado As String = System.Text.ASCIIEncoding.ASCII.GetString(data)
-        Dim areaId As Integer = Convert.ToInt32(idAreaDecodificado)
-        Return areaId
-    End Function
-
-    Protected Function decodificarIdCarpeta() As Integer
-        Dim idCarpetaCodificada As String = Request.QueryString("o").ToString()
-        Dim data() As Byte = System.Convert.FromBase64String(idCarpetaCodificada)
-        Dim idCarpetaDecodificada As String = System.Text.ASCIIEncoding.ASCII.GetString(data)
-        Dim carpetaId As Integer = Convert.ToInt32(idCarpetaDecodificada)
-        Return carpetaId
-    End Function
-
-    Protected Function decodificarRut() As Integer
-        Dim rutCodificado As String = Request.QueryString("p").ToString()
-        Dim data() As Byte = System.Convert.FromBase64String(rutCodificado)
-        Dim rutDecodificado As String = System.Text.ASCIIEncoding.ASCII.GetString(data)
-        Dim rutUsuario As Integer = Convert.ToInt32(rutDecodificado)
-        Return rutUsuario
-    End Function
-
-    Protected Sub cargarComentariosUrl()
-
-        Dim rutUsuario = decodificarRut()
-
-        Dim comentario As New clsComentario
-        Dim tarjeta As String = ""
-        Dim listaComentarios As DataTable = comentario.obtenerComentarios(decodificarIdArea(), decodificarIdDocumento(), decodificarIdCarpeta())
-
-        'Ordenando la lista de comentarios por fecha
-        Dim datav As New DataView
-        datav = listaComentarios.DefaultView
-        datav.Sort = "fecha"
-        listaComentarios = datav.ToTable()
-
-
-        'Dim Empresas As Object = crearEmpresas()
-
-        ' Ciclo for que recorre la lista de comentarios 
-        For Each fila As DataRow In listaComentarios.Rows
-
-            If fila("rutAutor") = rutUsuario Then
-                tarjeta = tarjeta & "  <div class=""row"">"
-                tarjeta = tarjeta & "   <div class=""col-2""></div>"
-                tarjeta = tarjeta & "   <div class=""col-10"">"
-                tarjeta = tarjeta & "    <div Class=""card shadow mb-4""> "
-                Dim nombre As String
-                nombre = obtenerNombreAutor(fila("rutAutor"))
-                Dim rol As String
-                rol = obtenerRolAutor(fila("rutAutor"))
-                tarjeta = tarjeta & "         <div Class=""card-header"">"
-                tarjeta = tarjeta & "           <div class=""row"">"
-                tarjeta = tarjeta & "               <div class=""m-0 font-weight-bold text-primary col-6"" >" + nombre + "/" + rol + "</div>"
-                tarjeta = tarjeta & "               <p class=""d-none d-lg-inline text-grey-600 small col-6 text-right"" >" + fila("fecha") + "</p>"
-                tarjeta = tarjeta & "           </div>"
-                tarjeta = tarjeta & "         </div>"
-                tarjeta = tarjeta & "         <div Class=""card-body"">"
-                Dim mensaje As String
-                mensaje = fila("texto")
-                tarjeta = tarjeta & "           <div>" + mensaje + "</div>"
-                tarjeta = tarjeta & "         </div> "
-                tarjeta = tarjeta & "      </div> "
-                tarjeta = tarjeta & "     </div> "
-                tarjeta = tarjeta & "    </div> "
-
-                lblTarjetaComentario.Text = tarjeta
-            Else
-                tarjeta = tarjeta & "  <div class=""col-10"">"
-                tarjeta = tarjeta & "   <div Class=""card shadow mb-4""> "
-                Dim nombre As String
-                nombre = obtenerNombreAutor(fila("rutAutor"))
-                Dim rol As String
-                rol = obtenerRolAutor(fila("rutAutor"))
-                tarjeta = tarjeta & "         <div Class=""card-header"">"
-                tarjeta = tarjeta & "           <div class=""row"">"
-                tarjeta = tarjeta & "               <div class=""m-0 font-weight-bold text-primary col-6"" >" + nombre + "/" + rol + "</div>"
-                tarjeta = tarjeta & "               <p class=""d-none d-lg-inline text-grey-600 small col-6 text-right"" >" + fila("fecha") + "</p>"
-                tarjeta = tarjeta & "           </div>"
-                tarjeta = tarjeta & "         </div>"
-                tarjeta = tarjeta & "           <div Class=""card-body"">"
-                Dim mensaje As String
-                mensaje = fila("texto")
-                tarjeta = tarjeta & "                     <div>" + mensaje + "</div>"
-                tarjeta = tarjeta & "        </div> "
-                tarjeta = tarjeta & "      </div> "
-                tarjeta = tarjeta & "     </div> "
-
-                lblTarjetaComentario.Text = tarjeta
-            End If
-
-        Next
 
     End Sub
 
