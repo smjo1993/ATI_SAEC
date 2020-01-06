4757a7e70a9145573f7e8fcbd956af5da5e2bd61
JorqueraFrancisco <33900932+JorqueraFrancisco@users.noreply.github.com>
2020-01-06 03:39:24 -0800

    comentarios

--- prueba_saec/prueba_saec/presentacion/Contratistas/subirDocumentos/listarTrabajadores.aspx.vb
+++ prueba_saec/prueba_saec/presentacion/Contratistas/subirDocumentos/listarTrabajadores.aspx.vb
@@ -2,26 +2,37 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
         sinTrabajadores.Visible = False
         validarContratista()
         If IsPostBack Then
             Return
         End If
+
+        'Se llama a la funcion para cargar el menu
         cargarMenu()
 
+        'Se crea el objeto trabajadores para luego mediante su rut llamar a listarTrabajadores y cargar la gridview
         Dim trabajadores = New clsTrabajador()
         Dim rutContratista As String = Session("contratistaEntrante").getRut
         Dim TablaTrabajadores As DataTable = trabajadores.listarTrabajadores(rutContratista)
         Session("rutEmpresa") = trabajadores.obtenerRutEmpresa(rutContratista).Rows(0).Item(0)
 
+        'Se valida si hay datos en el datatable
         If TablaTrabajadores Is Nothing Then
+
             sinTrabajadores.Visible = True
         Else
+
             If TablaTrabajadores.Rows.Count > 0 Then
+
                 gridListarTrabajadores.DataSource = TablaTrabajadores
                 gridListarTrabajadores.DataBind()
+
             Else
+
                 sinTrabajadores.Visible = True
+
             End If
         End If
 
@@ -45,7 +56,8 @@
     Protected Sub btnIrTrabajador_RowCommand(sender As Object, e As GridViewCommandEventArgs) Handles gridListarTrabajadores.RowCommand
 
         If (e.CommandName = "ir") Then
-            'Se obtienen los datos de la columna de la grid para mandar el datatable a la otra vista
+
+            'Se obtienen los datos de la columna del trabajador para mandarlas por session a la proxima vista
             Dim pos As Integer = Convert.ToInt32(e.CommandArgument.ToString())
             Dim rutTrabajador As String = gridListarTrabajadores.Rows(pos).Cells(0).Text
             Dim idCarpeta As Integer = gridListarTrabajadores.Rows(pos).Cells(2).Text
@@ -63,7 +75,7 @@
 
         If (e.CommandName = "eliminar") Then
 
-            'Se obtienen los datos de la columna de la grid para mandar el datatable a la otra vista   
+            'Se obtienen los datos de la columna para poder eliminar al trabajador selecionado y hacer reload a la misma pagina 
             Dim pos As Integer = Convert.ToInt32(e.CommandArgument.ToString())
             Dim idTrabajador As Integer = gridListarTrabajadores.Rows(pos).Cells(3).Text
             Dim trabajador = New clsTrabajador()
