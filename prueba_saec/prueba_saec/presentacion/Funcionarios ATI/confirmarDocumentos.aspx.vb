ea0d456d4f40ae94b3bb3818af4924399946a6ac
JorqueraFrancisco <33900932+JorqueraFrancisco@users.noreply.github.com>
2020-01-20 07:21:57 -0800

    Se corrige vista confirmar documentos

--- prueba_saec/prueba_saec/presentacion/Funcionarios ATI/confirmarDocumentos.aspx.vb
+++ prueba_saec/prueba_saec/presentacion/Funcionarios ATI/confirmarDocumentos.aspx.vb
@@ -15,44 +15,95 @@
         Dim usuario As clsUsuarioSAEC = Session("usuario")
         Session("rutUsuario") = usuario.getRut
         Dim idCarpeta As Integer = decodificarId()
-        Dim TablaDocumentosEsperaEmpresa As DataTable = crearDocumentos().obtenerDocumentoEstadoAplicaEmpresa(idCarpeta, usuario.getArea)
 
-        If (TablaDocumentosEsperaEmpresa Is Nothing) Then
-            sinDocEmpresa.Visible = True
-        Else
-            If (TablaDocumentosEsperaEmpresa.Rows.Count > 0) Then
-                confirmarEmpresa.DataSource = TablaDocumentosEsperaEmpresa
-                confirmarEmpresa.DataBind()
-            Else
+        Dim listaRoles As List(Of clsRol) = New List(Of clsRol)
+        listaRoles = Session("roles")
+
+        If listaRoles(0).getId = 1 Then
+
+            Dim TablaDocumentosEsperaEmpresa As DataTable = crearDocumentos().obtenerDocumentoEstadoAplicaEmpresaAdmin(idCarpeta, Nothing)
+
+            If (TablaDocumentosEsperaEmpresa Is Nothing) Then
                 sinDocEmpresa.Visible = True
-            End If
-        End If
-
-        Dim TablaDocumentosEsperaTrabajador As DataTable = crearDocumentos().obtenerDocumentoEstadoAplicaTrabajador(idCarpeta, usuario.getArea)
-
-        If (TablaDocumentosEsperaTrabajador Is Nothing) Then
-            sinDocTrabajador.Visible = True
-        Else
-            If (TablaDocumentosEsperaTrabajador.Rows.Count > 0) Then
-                confirmarTrabajador.DataSource = TablaDocumentosEsperaTrabajador
-                confirmarTrabajador.DataBind()
             Else
+                If (TablaDocumentosEsperaEmpresa.Rows.Count > 0) Then
+                    confirmarEmpresa.DataSource = TablaDocumentosEsperaEmpresa
+                    confirmarEmpresa.DataBind()
+                Else
+                    sinDocEmpresa.Visible = True
+                End If
+            End If
+
+            Dim TablaDocumentosEsperaTrabajador As DataTable = crearDocumentos().obtenerDocumentoEstadoAplicaTrabajadorAdmin(idCarpeta, Nothing)
+
+            If (TablaDocumentosEsperaTrabajador Is Nothing) Then
                 sinDocTrabajador.Visible = True
-            End If
-        End If
-
-        Dim TablaDocumentosEsperaVehiculo As DataTable = crearDocumentos().obtenerDocumentoEstadoAplicaVehiculo(idCarpeta, usuario.getArea)
-
-        If (TablaDocumentosEsperaVehiculo Is Nothing) Then
-            sinDocVehiculo.Visible = True
-        Else
-            If (TablaDocumentosEsperaVehiculo.Rows.Count > 0) Then
-                confirmarVehiculo.DataSource = TablaDocumentosEsperaVehiculo
-                confirmarVehiculo.DataBind()
             Else
-                sinDocVehiculo.Visible = True
+                If (TablaDocumentosEsperaTrabajador.Rows.Count > 0) Then
+                    confirmarTrabajador.DataSource = TablaDocumentosEsperaTrabajador
+                    confirmarTrabajador.DataBind()
+                Else
+                    sinDocTrabajador.Visible = True
+                End If
             End If
+
+            Dim TablaDocumentosEsperaVehiculo As DataTable = crearDocumentos().obtenerDocumentoEstadoAplicaVehiculoAdmin(idCarpeta, Nothing)
+
+            If (TablaDocumentosEsperaVehiculo Is Nothing) Then
+                sinDocVehiculo.Visible = True
+            Else
+                If (TablaDocumentosEsperaVehiculo.Rows.Count > 0) Then
+                    confirmarVehiculo.DataSource = TablaDocumentosEsperaVehiculo
+                    confirmarVehiculo.DataBind()
+                Else
+                    sinDocVehiculo.Visible = True
+                End If
+            End If
+
+        Else
+
+            Dim TablaDocumentosEsperaEmpresa As DataTable = crearDocumentos().obtenerDocumentoEstadoAplicaEmpresa(idCarpeta, usuario.getArea)
+
+            If (TablaDocumentosEsperaEmpresa Is Nothing) Then
+                sinDocEmpresa.Visible = True
+            Else
+                If (TablaDocumentosEsperaEmpresa.Rows.Count > 0) Then
+                    confirmarEmpresa.DataSource = TablaDocumentosEsperaEmpresa
+                    confirmarEmpresa.DataBind()
+                Else
+                    sinDocEmpresa.Visible = True
+                End If
+            End If
+
+            Dim TablaDocumentosEsperaTrabajador As DataTable = crearDocumentos().obtenerDocumentoEstadoAplicaTrabajador(idCarpeta, usuario.getArea)
+
+            If (TablaDocumentosEsperaTrabajador Is Nothing) Then
+                sinDocTrabajador.Visible = True
+            Else
+                If (TablaDocumentosEsperaTrabajador.Rows.Count > 0) Then
+                    confirmarTrabajador.DataSource = TablaDocumentosEsperaTrabajador
+                    confirmarTrabajador.DataBind()
+                Else
+                    sinDocTrabajador.Visible = True
+                End If
+            End If
+
+            Dim TablaDocumentosEsperaVehiculo As DataTable = crearDocumentos().obtenerDocumentoEstadoAplicaVehiculo(idCarpeta, usuario.getArea)
+
+            If (TablaDocumentosEsperaVehiculo Is Nothing) Then
+                sinDocVehiculo.Visible = True
+            Else
+                If (TablaDocumentosEsperaVehiculo.Rows.Count > 0) Then
+                    confirmarVehiculo.DataSource = TablaDocumentosEsperaVehiculo
+                    confirmarVehiculo.DataBind()
+                Else
+                    sinDocVehiculo.Visible = True
+                End If
+            End If
+
         End If
+
+
 
 
 
@@ -103,93 +154,101 @@
 
     End Function
 
-    Protected Sub btnConfirmarDocumentos_Click(sender As Object, e As EventArgs) Handles btnConfirmarDocumentos.Click
-        Dim contador As Integer = 0
-        Dim dt As DataTable = New DataTable("CambioEstado")
+    'Protected Sub btnConfirmarDocumentos_Click(sender As Object, e As EventArgs) Handles btnConfirmarDocumentos.Click
 
-        'Se recorre cada checkbox generado 
-        For Each fila As GridViewRow In confirmarEmpresa.Rows
+    '    Dim dt As DataTable = New DataTable("CambioEstado")
 
-            Dim idCarpeta As Integer = fila.Cells(2).Text
-            Dim idDocumento As Integer = fila.Cells(3).Text
-            Dim idArea As Integer = fila.Cells(4).Text
-            Dim check As HtmlInputCheckBox
-            Dim actualizarEstado = New clsDocumento()
+    '    'Se recorre cada checkbox generado 
+    '    For Each fila As GridViewRow In confirmarEmpresa.Rows
 
-            check = fila.FindControl("chk")
+    '        Dim idCarpeta As Integer = fila.Cells(2).Text
+    '        Dim idDocumento As Integer = fila.Cells(3).Text
+    '        Dim idArea As Integer = fila.Cells(4).Text
+    '        Dim check As HtmlInputCheckBox
+    '        Dim actualizarEstado = New clsDocumento()
 
-            'Si está check
-            If check.Checked Then
-                'Cambia el estado del documento a "pendiente"
-                actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "pendiente", Nothing)
-            Else
-                'Cambia el estado del documento a "no solicitado"
-                actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "espera", Nothing)
-            End If
+    '        check = fila.FindControl("chk")
 
-        Next
+    '        'Si está check
+    '        If check.Checked Then
+    '            'Cambia el estado del documento a "pendiente"
+    '            actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "pendiente", Nothing)
+    '        Else
+    '            'Cambia el estado del documento a "no solicitado"
+    '            actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "espera", Nothing)
+    '        End If
 
-        For Each fila As GridViewRow In confirmarTrabajador.Rows
+    '    Next
 
-            Dim idCarpeta As Integer = fila.Cells(2).Text
-            Dim idDocumento As Integer = fila.Cells(3).Text
-            Dim idArea As Integer = fila.Cells(4).Text
-            Dim check As HtmlInputCheckBox
-            Dim actualizarEstado = New clsDocumento()
+    '    For Each fila As GridViewRow In confirmarTrabajador.Rows
 
-            check = fila.FindControl("chk")
+    '        Dim idCarpeta As Integer = fila.Cells(2).Text
+    '        Dim idDocumento As Integer = fila.Cells(3).Text
+    '        Dim idArea As Integer = fila.Cells(4).Text
+    '        Dim check As HtmlInputCheckBox
+    '        Dim actualizarEstado = New clsDocumento()
 
-            'Si está check
-            If check.Checked Then
-                'Cambia el estado del documento a "pendiente"
-                actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "pendiente", Nothing)
-            Else
-                'Cambia el estado del documento a "no solicitado"
-                actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "espera", Nothing)
-            End If
-        Next
+    '        check = fila.FindControl("chk")
 
-        For Each fila As GridViewRow In confirmarVehiculo.Rows
+    '        'Si está check
+    '        If check.Checked Then
+    '            'Cambia el estado del documento a "pendiente"
+    '            actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "pendiente", Nothing)
+    '        Else
+    '            'Cambia el estado del documento a "no solicitado"
+    '            actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "espera", Nothing)
+    '        End If
+    '    Next
 
-            Dim idCarpeta As Integer = fila.Cells(2).Text
-            Dim idDocumento As Integer = fila.Cells(3).Text
-            Dim idArea As Integer = fila.Cells(4).Text
-            Dim check As HtmlInputCheckBox
-            Dim actualizarEstado = New clsDocumento()
+    '    For Each fila As GridViewRow In confirmarVehiculo.Rows
 
-            check = fila.FindControl("chk")
+    '        Dim idCarpeta As Integer = fila.Cells(2).Text
+    '        Dim idDocumento As Integer = fila.Cells(3).Text
+    '        Dim idArea As Integer = fila.Cells(4).Text
+    '        Dim check As HtmlInputCheckBox
+    '        Dim actualizarEstado = New clsDocumento()
 
-            'Si está check
-            If check.Checked Then
-                'Cambia el estado del documento a "pendiente"
-                actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "pendiente", Nothing)
-            Else
-                'Cambia el estado del documento a "no solicitado"
-                actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "espera", Nothing)
-            End If
+    '        check = fila.FindControl("chk")
 
-        Next
-        Response.Redirect("verCarpetas.aspx")
-    End Sub
+    '        'Si está check
+    '        If check.Checked Then
+    '            'Cambia el estado del documento a "pendiente"
+    '            actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "pendiente", Nothing)
+    '        Else
+    '            'Cambia el estado del documento a "no solicitado"
+    '            actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "espera", Nothing)
+    '        End If
+
+    '    Next
+    '    Response.Redirect("verCarpetas.aspx")
+    'End Sub
 
     Protected Sub documentosEmpresa_RowCommand(sender As Object, e As GridViewCommandEventArgs) Handles confirmarEmpresa.RowCommand
 
+        Dim pos As Integer = Convert.ToInt32(e.CommandArgument.ToString())
+        Dim idCarpeta As Integer = confirmarEmpresa.Rows(pos).Cells(2).Text
+        Dim idDocumento As Integer = confirmarEmpresa.Rows(pos).Cells(3).Text
+        Dim idArea As Integer = confirmarEmpresa.Rows(pos).Cells(4).Text
+        Dim actualizarEstado = New clsDocumento()
+
+        If (e.CommandName = "aceptar") Then
+
+            actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "pendiente", Nothing)
+            Response.Redirect(HttpContext.Current.Request.Url.ToString)
+
+        End If
+
         If (e.CommandName = "eliminar") Then
 
-            Dim pos As Integer = Convert.ToInt32(e.CommandArgument.ToString())
-            Dim idCarpeta As Integer = confirmarEmpresa.Rows(pos).Cells(2).Text
-            Dim idDocumento As Integer = confirmarEmpresa.Rows(pos).Cells(3).Text
-            Dim idArea As Integer = confirmarEmpresa.Rows(pos).Cells(4).Text
-            Dim actualizarEstado = New clsDocumento()
+
 
             actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "no solicitado", Nothing)
             Response.Redirect(HttpContext.Current.Request.Url.ToString)
 
         End If
 
-        If (e.CommandName = "Ver") Then
+        If (e.CommandName = "ver") Then
 
-            Dim pos As Integer = Convert.ToInt32(e.CommandArgument.ToString())
             Dim areaId As String = confirmarEmpresa.Rows(pos).Cells(4).Text
             Dim documentoId As String = confirmarEmpresa.Rows(pos).Cells(3).Text
             Dim carpetaId As String = confirmarEmpresa.Rows(pos).Cells(2).Text
@@ -207,13 +266,22 @@
 
     Protected Sub confirmarTrabajador_RowCommand(sender As Object, e As GridViewCommandEventArgs) Handles confirmarTrabajador.RowCommand
 
+        Dim pos As Integer = Convert.ToInt32(e.CommandArgument.ToString())
+        Dim idCarpeta As Integer = confirmarTrabajador.Rows(pos).Cells(2).Text
+        Dim idDocumento As Integer = confirmarTrabajador.Rows(pos).Cells(3).Text
+        Dim idArea As Integer = confirmarTrabajador.Rows(pos).Cells(4).Text
+        Dim actualizarEstado = New clsDocumento()
+
+        If (e.CommandName = "aceptar") Then
+
+            actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "pendiente", Nothing)
+            Response.Redirect(HttpContext.Current.Request.Url.ToString)
+
+        End If
+
         If (e.CommandName = "eliminar") Then
 
-            Dim pos As Integer = Convert.ToInt32(e.CommandArgument.ToString())
-            Dim idCarpeta As Integer = confirmarTrabajador.Rows(pos).Cells(2).Text
-            Dim idDocumento As Integer = confirmarTrabajador.Rows(pos).Cells(3).Text
-            Dim idArea As Integer = confirmarTrabajador.Rows(pos).Cells(4).Text
-            Dim actualizarEstado = New clsDocumento()
+
 
             actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "no solicitado", Nothing)
             Response.Redirect(HttpContext.Current.Request.Url.ToString)
@@ -222,7 +290,6 @@
 
         If (e.CommandName = "Ver") Then
 
-            Dim pos As Integer = Convert.ToInt32(e.CommandArgument.ToString())
             Dim areaId As String = confirmarTrabajador.Rows(pos).Cells(4).Text
             Dim documentoId As String = confirmarTrabajador.Rows(pos).Cells(3).Text
             Dim carpetaId As String = confirmarTrabajador.Rows(pos).Cells(2).Text
@@ -237,22 +304,29 @@
     End Sub
     Protected Sub confirmarVehiculo_RowCommand(sender As Object, e As GridViewCommandEventArgs) Handles confirmarVehiculo.RowCommand
 
-        If (e.CommandName = "eliminar") Then
+        Dim pos As Integer = Convert.ToInt32(e.CommandArgument.ToString())
+        Dim idCarpeta As Integer = confirmarVehiculo.Rows(pos).Cells(2).Text
+        Dim idDocumento As Integer = confirmarVehiculo.Rows(pos).Cells(3).Text
+        Dim idArea As Integer = confirmarVehiculo.Rows(pos).Cells(4).Text
+        Dim actualizarEstado = New clsDocumento()
 
-            Dim pos As Integer = Convert.ToInt32(e.CommandArgument.ToString())
-            Dim idCarpeta As Integer = confirmarVehiculo.Rows(pos).Cells(2).Text
-            Dim idDocumento As Integer = confirmarVehiculo.Rows(pos).Cells(3).Text
-            Dim idArea As Integer = confirmarVehiculo.Rows(pos).Cells(4).Text
-            Dim actualizarEstado = New clsDocumento()
+
+        If (e.CommandName = "eliminar") Then
 
             actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "no solicitado", Nothing)
             Response.Redirect(HttpContext.Current.Request.Url.ToString)
 
         End If
 
+        If (e.CommandName = "aceptar") Then
+
+            actualizarEstado.cambiarEstadoDocumento(idCarpeta, idArea, idDocumento, "pendiente", Nothing)
+            Response.Redirect(HttpContext.Current.Request.Url.ToString)
+
+        End If
+
         If (e.CommandName = "Ver") Then
 
-            Dim pos As Integer = Convert.ToInt32(e.CommandArgument.ToString())
             Dim areaId As String = confirmarVehiculo.Rows(pos).Cells(4).Text
             Dim documentoId As String = confirmarVehiculo.Rows(pos).Cells(3).Text
             Dim carpetaId As String = confirmarVehiculo.Rows(pos).Cells(2).Text
@@ -261,6 +335,7 @@
             Session("carpetaId") = carpetaId
             Session("origen") = HttpContext.Current.Request.Url.ToString
             Response.Redirect("../Contratistas/verComentarios.aspx")
+
         End If
 
 
